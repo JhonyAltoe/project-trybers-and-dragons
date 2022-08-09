21a3bd16c7dd539d5084f77d8ec31b4434ff1a87
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-08-09 17:09:45 -0300

    Feat: Implementa classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,17 @@
+export default abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+  abstract get maxLifePoints(): number;
+  
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name() { return this._name; }
+  get dexterity() { return this._dexterity; }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
